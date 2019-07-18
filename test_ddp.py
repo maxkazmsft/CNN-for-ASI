47bbafdd5a391c1c9e149e58b05bc19055e9a844
Max Kaznady <maxkaz@microsoft.com>
2019-07-18 17:30:25 +0000

    temp commit while moving machines

--- test_ddp.py
+++ test_ddp.py
@@ -1,4 +1,38 @@
 import os
+
+NGPU = 8
+DEVICE_IDS = list(range(NGPU))
+os.environ["CUDA_VISIBLE_DEVICES"] = ",".join([str(x) for x in DEVICE_IDS])
+
+import torch
+import torch.distributed as dist
+from torch.multiprocessing import Process
+
+def run(rank, size):
+    """ Distributed function to be implemented later. """
+    pass
+
+def init_processes(rank, size, fn, backend='tcp'):
+    """ Initialize the distributed environment. """
+    os.environ['MASTER_ADDR'] = '127.0.0.1'
+    os.environ['MASTER_PORT'] = '29500'
+    dist.init_process_group(backend, rank=rank, world_size=size)
+    fn(rank, size)
+
+
+if __name__ == "__main__":
+    size = 2
+    processes = []
+    for rank in range(size):
+        p = Process(target=init_processes, args=(rank, size, run))
+        p.start()
+        processes.append(p)
+
+    for p in processes:
+        p.join()
+
+
+"""
 import tempfile
 import torch
 import torch.distributed as dist
@@ -72,3 +106,5 @@ if __name__ == "__main__":
 
     #if torch.cuda.device_count() >= 8:
     #    run_demo(demo_model_parallel, 4)
+
+"""
